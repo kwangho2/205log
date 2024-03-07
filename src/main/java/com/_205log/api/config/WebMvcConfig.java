f04571a7558d5b28d9b84c0df812e2bd2ecde24d
KwangHo Lee <ohgwang90@naver.com>
2024-03-07 15:58:21 +0900

    파라미터에서 헤더인증으로 변경, Intellij http 추가

--- src/main/java/com/_205log/api/config/WebMvcConfig.java
+++ src/main/java/com/_205log/api/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com._205log.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
